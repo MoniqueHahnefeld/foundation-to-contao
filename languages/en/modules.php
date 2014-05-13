4716dee2daa7aaffa0aab8196c4168d71e0ed317
Monique Hahnefeld <info@monique-hahnefeld.de>
2014-05-13 09:41:48 +0200

    add language files

---
+++ languages/en/modules.php
@@ -0,0 +1,17 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   Foundation To Contao
+ * @author    Monique Hahnefeld
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ * @copyright 2014 Monique Hahnefeld
+ */
+ 
+
+$GLOBALS['TL_LANG']['MOD']['foundation-to-contao'][0] = 'FTC - Foundation To Contao';
+
+
+?>
